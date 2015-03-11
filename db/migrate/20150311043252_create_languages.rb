d928fa1f4f521674c0a21133fa6f7318af6bacda
Xiao Yu Zhu <cj_zhuyu@yahoo.com>
2015-03-11 10:59:07 -0400

    generate models

--- db/migrate/20150311043252_create_languages.rb
+++ db/migrate/20150311043252_create_languages.rb
@@ -1,9 +1,9 @@
 class CreateLanguages < ActiveRecord::Migration
   def change
     create_table :languages do |t|
-      t.string :language
+      t.string :name
       t.string :code
-
+      t.boolean :default, default: false
       t.timestamps null: false
     end
   end
